b3dede067be5fdfccf858b75ea5d2912e64e06a3
GIREESH V PAI <gireeshvpai2007@gmail.com>
2026-08-16 23:15:05 +0530

    Time: 20 ms (95.15%), Space: 82.8 MB (87.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
 bool containsDuplicate(vector<int>& nums) {
-    unordered_set<int> s;
+    sort(nums.begin(), nums.end());
 
-    for (int x : nums) {
-        if (s.count(x))
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] == nums[i - 1])
             return true;
-
-        s.insert(x);
     }
 
     return false;
-
-    }
+}
 };
